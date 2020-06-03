197a4780a4e4c11f901e3f1284902f741b6f011c
GrygrFlzr <grygrflzr@hotmail.com>
2020-06-03 22:31:25 +0700

    Specify easing for crossfade

--- src/transition.js
+++ src/transition.js
@@ -1,6 +1,9 @@
 import { crossfade, scale } from 'svelte/transition';
+import { cubicInOut } from 'svelte/easing';
 
 export const transition = crossfade({
+	delay: 0,
 	duration: 500,
-	fallback: scale
+	fallback: scale,
+	easing: cubicInOut
 });
