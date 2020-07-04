8d3780cd515f5e6b0137452d7142e185d2a5cafb
GrygrFlzr <grygrflzr@hotmail.com>
2020-07-04 13:26:24 +0700

    [data] Added images for AOF
    Reduced Ashley image size

--- src/helpers/stores.js
+++ src/helpers/stores.js
@@ -42,11 +42,14 @@ const events = [
 		title: "Aniplex Online Fest",
 		start: "04 July 2020 09:30:00 PDT",
 		end: "05 July 2020 23:00:00 PDT",
+		image: "/images/aof.png",
 	},
 	{
 		title: "Aniplex Online Fest: Magia Record Panel",
 		start: "04 July 2020 12:00:00 PDT",
 		end: "04 July 2020 12:45:00 PDT",
+		image: "/images/aof_magireco.png",
+		theme: "dark",
 	},
 	{
 		title: "July 07 Maintenance",
