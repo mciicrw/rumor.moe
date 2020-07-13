0ca7ee6be58eac8756a754f2427d507212903617
GrygrFlzr <grygrflzr@hotmail.com>
2020-07-13 17:22:35 +0700

    [data] One Fleeting Summer Night

--- src/helpers/stores.js
+++ src/helpers/stores.js
@@ -4,45 +4,32 @@ import { readable } from 'svelte/store';
 
 const events = [
 	{
-		title: "1 Year Anniversary Campaign",
-		start: "25 June 2020 02:00:00 PDT",
-		end: "06 July 2020 23:59:59 PDT",
-		theme: "dark",
-	},
-	{
-		title: "Cherry Blossom Dreams",
-		start: "29 June 2020 02:00:00 PDT",
-		end: "06 July 2020 23:59:59 PDT",
-		image: "/images/card_30435_s.png",
-	},
-	{
-		title: "Ashley Taylor Fate Weave",
-		start: "29 June 2020 02:00:00 PDT",
-		end: "06 July 2020 23:59:59 PDT",
-		image: "/images/card_30524_s.png",
-	},
-	{
-		title: "1 Year Anniversary Login Bonus",
-		start: "29 June 2020 02:00:00 PDT",
-		end: "06 July 2020 23:59:59 PDT",
-		theme: "dark",
-	},
-	{
 		title: "July Shop",
 		start: "01 July 2020 00:00:00 PDT",
 		end: "31 July 2020 23:59:59 PDT",
 		image: "/images/card_10174_s.png",
 	},
 	{
-		title: "July 07 Maintenance",
-		start: "07 July 2020 00:00:00 PDT",
-		end: "07 July 2020 03:00:00 PDT",
-	},
-	{
 		title: "A Fledgling's First Flight",
 		start: "07 July 2020 03:00:00 PDT",
 		end: "14 July 2020 23:59:59 PDT",
 		image: "/images/card_10155_s.png",
+	},
+	{
+		title: "July 15 Maintenance",
+		start: "15 July 2020 00:00:00 PDT",
+		end: "15 July 2020 02:00:00 PDT",
+	},
+	{
+		title: "One Fleeting Summer Night",
+		start: "15 July 2020 03:00:00 PDT",
+		end: "21 July 2020 23:59:59 PDT",
+		image: "/images/card_11184_s.png",
+	},
+	{
+		title: "July 22 Maintenance",
+		start: "22 July 2020 00:00:00 PDT",
+		end: "22 July 2020 02:00:00 PDT",
 	},
 ];
 
