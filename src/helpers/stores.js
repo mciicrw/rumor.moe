7bfa3fd60553650a08d1ba3823914a0ebdbd869e
GrygrFlzr <grygrflzr@hotmail.com>
2020-07-20 14:26:29 +0700

    [data] Added Rebel of a Dawnless Land

--- src/helpers/stores.js
+++ src/helpers/stores.js
@@ -31,6 +31,29 @@ const events = [
 		start: "22 July 2020 00:00:00 PDT",
 		end: "22 July 2020 02:00:00 PDT",
 	},
+	{
+		title: "Rebel of a Dawnless Land",
+		start: "22 July 2020 03:00:00 PDT",
+		end: "02 August 2020 23:59:59 PDT",
+		image: "/images/card_40264_s.png",
+	},
+	{
+		title: "Melissa Uncap",
+		start: "22 July 2020 03:00:00 PDT",
+		end: null,
+		image: "/images/card_40235_s.png",
+	},
+	{
+		title: "August Shop",
+		start: "01 August 2020 00:00:00 PDT",
+		end: "31 August 2020 23:59:59 PDT",
+		image: "/images/card_10174_s.png",
+	},
+	{
+		title: "August 03 Maintenance",
+		start: "03 August 2020 00:00:00 PDT",
+		end: "03 August 2020 02:00:00 PDT",
+	},
 ];
 
 let id = 0;
